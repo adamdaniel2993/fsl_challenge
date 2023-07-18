ea64e7277dce08ab65d545abf2b3ffeecb41bdcb
Adan Daniel <axd3@cbord.com>
2023-07-17 22:21:23 -0400

    terraform

---
+++ terraform/amplify.tf
@@ -0,0 +1,43 @@
+resource "aws_amplify_app" "fsl_react_app" {
+  name       = "fsl-react-app-${var.environment}"
+  repository = "https://github.com/adamdaniel2993/fsl_challenge"
+
+  # The default build_spec added by the Amplify Console for React.
+  build_spec = <<-EOT
+    version: 1
+    frontend:
+      phases:
+        preBuild:
+          commands:
+            - nvm install 15
+            - nvm use 15
+            - npm install 
+        build:
+          commands:
+            - npm run build
+      artifacts:
+        baseDirectory: build
+        files:
+          - '**/*'
+      cache:
+        paths:
+          - node_modules/**/*
+  EOT
+
+  # The default rewrites and redirects added by the Amplify Console.
+  custom_rule {
+    source = "/<*>"
+    status = "404"
+    target = "/index.html"
+  }
+
+}
+
+resource "aws_amplify_branch" "main" {
+  app_id      = aws_amplify_app.fsl_react_app.id
+  branch_name = "main"
+
+  framework = "React"
+  stage     = terraform.workspace
+
+}
